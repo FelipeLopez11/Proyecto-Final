cd4006cb722d1acc4da737b60a07af434ce017ee
FelipeLopez11 <jfelipelopez8@gmail.com>
2025-06-06 18:17:57 -0500

    Añadida la parte de galeria, añadido nuevo navbar a todas las paginas, solucionado el active en links

--- js/validar_botones.js
+++ js/validar_botones.js
@@ -6,19 +6,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (loggedInStatus === 'true') {
         // Si está logueado:
-        if (iniciar_sesionBtn) iniciar_sesionBtn.classList.add('hidden');       
-        if (cerrar_sesionBtn) cerrar_sesionBtn.classList.remove('hidden'); 
+        if (iniciar_sesionBtn) iniciar_sesionBtn.classList.add('hidden');
+        if (cerrar_sesionBtn) cerrar_sesionBtn.classList.remove('hidden');
     } else {
         // Si NO está logueado:
-        if (iniciar_sesionBtn) iniciar_sesionBtn.classList.remove('hidden');    
-        if (cerrar_sesionBtn) cerrar_sesionBtn.classList.add('hidden');  
+        if (iniciar_sesionBtn) iniciar_sesionBtn.classList.remove('hidden');
+        if (cerrar_sesionBtn) cerrar_sesionBtn.classList.add('hidden');
     }
 
     // Lógica para el botón de Cerrar Sesión en index.html
     if (cerrar_sesionBtn) {
         cerrar_sesionBtn.addEventListener('click', () => {
-            localStorage.removeItem('logStatus'); 
-            location.reload(); 
+            localStorage.removeItem('logStatus');
+            location.reload();
         });
     }
+
+    const currentPath = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll('.nav-menu a'); 
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+
+        const linkPath = link.getAttribute('href'); 
+
+        // Compara si el 'href' del enlace coincide con la ruta actual
+        // o si es la página de inicio (index.html) cuando la ruta es la raíz ('')
+        if (linkPath === currentPath || (currentPath === '' && linkPath === 'index.html')) {
+            link.classList.add('active');
+        }
+    });
 });
+
